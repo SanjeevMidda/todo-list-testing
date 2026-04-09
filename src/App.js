202ddc87a003ec452009e64e089c1d70fee50d5e
Sanjeev Midda <s.midda1@gmail.com>
2026-04-09 18:21:28 +0100

    added requirements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./index.css";
 
 function App() {
+  // requirements
+  // add a todo
+  // delete a todo
+  // undo a deleted todo item
+  // filter todo by type
+
   return <div className="App"></div>;
 }
 
